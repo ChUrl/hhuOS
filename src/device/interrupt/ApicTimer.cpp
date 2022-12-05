59c8753269b5042c105120365e81b5a0f98f3dca
ChUrl <christoph.urlacher@protonmail.com>
2022-12-05 21:41:35 +0100

    Update ApicTimer implementation

--- src/device/interrupt/ApicTimer.cpp
+++ src/device/interrupt/ApicTimer.cpp
@@ -8,20 +8,18 @@ Kernel::Logger ApicTimer::log = Kernel::Logger::get("ApicTimer");
 ApicTimer::ApicTimer() {
     // Recommended order: Divide -> LVT -> Initial Count (OSDev)
     LApic::writeDoubleWord(LApic::Register::TIMER_DIVIDE, Divide::BY_1);
-    LApic::writeLVT(LApic::TIMER, { .slot = Kernel::InterruptDispatcher::APICTIMER,
-                                    .isMasked = true,
-                                    .timerMode = LApic::LVT_Timer_Mode::PERIODIC });
+    LApic::writeLVT(LApic::TIMER, { .timerMode = LApic::LVT_Timer_Mode::PERIODIC });
 
     // TODO: Has to be calibrated (using PIT or RTC or probably just the TimeService)
-    // NOTE: Setting this to values smaller than 0xFFFFFFFF doesn't work?
-    // NOTE: Probably because the timer interrupt is bugged so if the timer counts down too fast it burns
+    // TODO: Setting this to values smaller than 0xFFFFFFFF doesn't work?
+    //       Probably because the timer interrupt is bugged so if the timer counts down too fast it burns
     LApic::writeDoubleWord(LApic::Register::TIMER_INITIAL, 0xFFFFFF);
 }
 
 void ApicTimer::plugin() {
     auto &interruptService = Kernel::System::getService<Kernel::InterruptService>();
     interruptService.assignInterrupt(Kernel::InterruptDispatcher::APICTIMER, *this);
-    LApic::allow(LApic::TIMER); // TODO: This should be done in interruptService
+    LApic::allow(LApic::TIMER, Kernel::InterruptDispatcher::APICTIMER); // TODO: This in interruptservice?
 }
 
 void ApicTimer::trigger(const Kernel::InterruptFrame &frame) {
