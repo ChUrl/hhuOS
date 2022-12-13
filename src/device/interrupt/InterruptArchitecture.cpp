a591a484e95862db1bb1b5903b6ea4d6dc849387
ChUrl <christoph.urlacher@protonmail.com>
2022-12-13 18:31:14 +0100

    Move GlobalSystemInterrupt into the InterruptArchitecture

--- src/device/interrupt/InterruptArchitecture.cpp
+++ src/device/interrupt/InterruptArchitecture.cpp
@@ -4,13 +4,87 @@ namespace Device {
 
 bool InterruptArchitecture::initialized;
 InterruptArchitecture::IntArch InterruptArchitecture::runningArchitecture = IntArch::PIC;
-
-InterruptArchitecture::LPlatformInformation *InterruptArchitecture::localPlatform =
-        new InterruptArchitecture::LPlatformInformation;
-InterruptArchitecture::IoPlatformInformation *InterruptArchitecture::ioPlatform =
-        new InterruptArchitecture::IoPlatformInformation;
-
+LPlatformInformation *InterruptArchitecture::localPlatform = new LPlatformInformation;
+IoPlatformInformation *InterruptArchitecture::ioPlatform = new IoPlatformInformation;
 Kernel::Logger InterruptArchitecture::log = Kernel::Logger::get("InterruptArchitecture");
+
+// ! Global system interrupts
+
+GlobalSystemInterrupt::GlobalSystemInterrupt(uint8_t gsi) : gsi(static_cast<Gsi>(gsi)) {}
+
+GlobalSystemInterrupt::GlobalSystemInterrupt(Kernel::InterruptDispatcher::Interrupt vector) {
+    if (vector < Kernel::InterruptDispatcher::PIT
+    || vector > static_cast<Kernel::InterruptDispatcher::Interrupt>(getGlobalGsiMax() + 32)) {
+        Util::Exception::throwException(Util::Exception::INVALID_ARGUMENT, "Vector does not belong to a hardware interrupt!");
+    }
+
+    gsi = static_cast<Gsi>(vector - Kernel::InterruptDispatcher::PIT);
+}
+
+GlobalSystemInterrupt::GlobalSystemInterrupt(InterruptInput inti) {
+    if (inti > static_cast<uint8_t>(getGlobalGsiMax())) {
+        Util::Exception::throwException(Util::Exception::INVALID_ARGUMENT, "INTI is not supported by the system!");
+    }
+
+    if (runningArchitecture == PIC) {
+        gsi = static_cast<Gsi>(inti);
+    }
+
+    if (gsi <= 15) {
+        verifyApic();
+        gsi = ioPlatform->intiToGsiMappings[inti];
+        if (gsi == 0xFF) {
+            Util::Exception::throwException(Util::Exception::UNSUPPORTED_OPERATION, "INTI is invalid!");
+        }
+    }
+
+    gsi = static_cast<Gsi>(inti);
+}
+
+bool GlobalSystemInterrupt::isValid() {
+    return ioPlatform->gsiToIntiMappings[gsi] != 0xFF;
+}
+
+GlobalSystemInterrupt::operator Gsi() const {
+    return gsi;
+}
+
+GlobalSystemInterrupt::operator Kernel::InterruptDispatcher::Interrupt() const {
+    auto vector = static_cast<Kernel::InterruptDispatcher::Interrupt>(gsi + Kernel::InterruptDispatcher::PIT);
+    if (vector < Kernel::InterruptDispatcher::PIT
+    || vector > static_cast<Kernel::InterruptDispatcher::Interrupt>(getGlobalGsiMax() + 32)) {
+        Util::Exception::throwException(Util::Exception::UNSUPPORTED_OPERATION, "Vector does not belong to a hardware interrupt!");
+    }
+    return vector;
+}
+
+GlobalSystemInterrupt::operator InterruptInput() const {
+    if (gsi > getGlobalGsiMax()) {
+        Util::Exception::throwException(Util::Exception::UNSUPPORTED_OPERATION, "INTI is not supported by the system!");
+    }
+
+    if (runningArchitecture == PIC) {
+        return static_cast<InterruptInput>(gsi);
+    }
+
+    // Devices connected to the PIC don't have to be connected to the same pins on the IO APIC
+    if (gsi <= 15) {
+        verifyApic();
+        InterruptInput inti = ioPlatform->gsiToIntiMappings[gsi];
+        if (inti == 0xFF) {
+            Util::Exception::throwException(Util::Exception::UNSUPPORTED_OPERATION, "INTI is invalid!");
+        }
+        return inti;
+    }
+
+    // Devices that are not connected to the PIC don't matter
+    return static_cast<InterruptInput>(gsi);
+}
+
+GlobalSystemInterrupt &GlobalSystemInterrupt::operator++() {
+    gsi = static_cast<Gsi>(gsi + 1);
+    return *this;
+}
 
 // ! Public member functions start here
 
@@ -20,76 +94,15 @@ void InterruptArchitecture::verifyInitialized() {
     }
 }
 
-bool InterruptArchitecture::hasApic() {
-    verifyInitialized();
-    return runningArchitecture == APIC;
-}
-
 void InterruptArchitecture::verifyApic() {
-    if (!hasApic()) {
+    verifyInitialized();
+    if (runningArchitecture != APIC) {
         Util::Exception::throwException(Util::Exception::ILLEGAL_STATE, "System is not running in APIC mode!");
     }
 }
 
-InterruptArchitecture::IntArch InterruptArchitecture::getRunningArchitecture() {
-    return runningArchitecture;
-}
-
-void InterruptArchitecture::dumpPlatformInformation() {
-    verifyInitialized();
-
-    if (runningArchitecture == PIC) {
-        log.info("Running in PIC mode");
-        return;
-    }
-
-    log.info("Running in APIC mode");
-
-    // Local APICs
-    log.info("Local Apic information:");
-    log.info("Supported modes: %s%s, Selected: %s, Cores: [%d], Version: [0x%x]",
-             localPlatform->xApicSupported ? "[xApic]" : "[None]", localPlatform->x2ApicSupported ? " [x2Apic]" : "",
-             localPlatform->isX2Apic ? "[x2Apic]" : "[xApic]", localPlatform->lapics.size(), localPlatform->version);
-    log.info("MMIO: [0x%x] (phys) -> [0x%x] (virt)", localPlatform->address, localPlatform->virtAddress);
-
-    log.info("Local Apic status:");
-    for (auto *lapic : localPlatform->lapics) {
-        log.info("- Id: [0x%x], Enabled: [%d]", lapic->id, lapic->enabled);
-    }
-
-    log.info("Local NMI status:");
-    for (auto *lnmi : localPlatform->lnmis) {
-        log.info("- Id: [0x%x], Lint: [LINT%d]", lnmi->id, lnmi->lint);
-    }
-
-    // IO APICs
-    log.info("Io Apic information:");
-    log.info("Version: [0x%x], EOI supported: [%d], Global GSI max: [%d]",
-             ioPlatform->version, ioPlatform->eoiSupported, static_cast<uint8_t>(ioPlatform->globalGsiMax));
-
-    log.info("Io Apic status:");
-    for (auto *ioapic : ioPlatform->ioapics) {
-        log.info("- Id: [0x%x], MMIO: [0x%x] (phys) -> [0x%x] (virt), GSI base: [%d], GSI max: [%d]",
-                 ioapic->id, ioapic->address, ioapic->virtAddress,
-                 static_cast<uint8_t>(ioapic->gsiBase), static_cast<uint8_t>(ioapic->gsiMax));
-    }
-
-    log.info("Io IRQ overrides:");
-    for (auto *override : ioPlatform->irqOverrides) {
-        log.info("- IRQ source: [%d], GSI target: [%d]",
-                 static_cast<uint8_t>(override->source), static_cast<uint8_t>(override->target));
-    }
-    if (ioPlatform->irqOverrides.size() == 0) {
-        log.info("- There are no IRQ overrides");
-    }
-
-    log.info("Io NMI status:");
-    for (auto *nmi : ioPlatform->ionmis) {
-        log.info("- GSI: [%d]", static_cast<uint8_t>(nmi->gsi));
-    }
-    if (ioPlatform->ionmis.size() == 0) {
-        log.info("- There are no IO NMIs");
-    }
+bool InterruptArchitecture::hasApic() noexcept {
+    return runningArchitecture == APIC;
 }
 
 GlobalSystemInterrupt InterruptArchitecture::getGlobalGsiMax() {
@@ -105,6 +118,56 @@ Util::Data::ArrayList<InterruptArchitecture::LApicInformation *> &InterruptArchi
 Util::Data::ArrayList<InterruptArchitecture::IoApicInformation *> &InterruptArchitecture::ioapics() {
     verifyApic();
     return ioPlatform->ioapics;
+}
+
+void InterruptArchitecture::dumpPlatformInformation() {
+    verifyInitialized();
+
+    if (runningArchitecture == PIC) {
+        log.info("Running in PIC mode");
+        return;
+    }
+
+    log.info("Running in APIC mode");
+
+    // Local APICs
+    log.info("Local APIC: Supported modes: [%s%s], Selected mode: [%s], Cores: [%d], Version: [0x%x]",
+             localPlatform->xApicSupported ? "xApic" : "None", localPlatform->x2ApicSupported ? ",x2Apic" : "",
+             localPlatform->isX2Apic ? "x2Apic" : "xApic", localPlatform->lapics.size(), localPlatform->version);
+    log.info("Local APIC MMIO: [0x%x] (phys) -> [0x%x] (virt)", localPlatform->address, localPlatform->virtAddress);
+
+    for (auto *lapic : localPlatform->lapics) {
+        log.info("- LApic: Id: [0x%x], Enabled: [%d]", lapic->id, lapic->enabled);
+    }
+
+    for (auto *lnmi : localPlatform->lnmis) {
+        log.info("- NMI: Id: [0x%x], Lint: [LINT%d]", lnmi->id, lnmi->lint);
+    }
+
+    // IO APICs
+    log.info("Io APIC: Version: [0x%x], EOI supported: [%d], Global GSI max: [%d]",
+             ioPlatform->version, ioPlatform->eoiSupported, static_cast<uint8_t>(ioPlatform->globalGsiMax));
+
+    for (auto *ioapic : ioPlatform->ioapics) {
+        log.info("- IoApic: Id: [0x%x], MMIO: [0x%x] (phys) -> [0x%x] (virt), GSI base: [%d], GSI max: [%d]",
+                 ioapic->id, ioapic->address, ioapic->virtAddress,
+                 static_cast<uint8_t>(ioapic->gsiBase), static_cast<uint8_t>(ioapic->gsiMax));
+    }
+
+    for (auto *override : ioPlatform->irqOverrides) {
+        log.info("- Override: Source: [%d], Target: [%d]",
+                 static_cast<uint8_t>(override->source), static_cast<uint8_t>(override->target));
+    }
+    if (ioPlatform->irqOverrides.size() == 0) {
+        log.info("- There are no IRQ overrides");
+    }
+
+    for (auto *nmi : ioPlatform->ionmis) {
+        log.info("- NMI: GSI: [%d]", static_cast<uint8_t>(nmi->gsi));
+    }
+    if (ioPlatform->ionmis.size() == 0) {
+        log.info("- There are no IO NMIs");
+    }
 }
 
 // ! Private member functions start here
@@ -135,11 +198,14 @@ InterruptArchitecture::LNMIConfiguration *InterruptArchitecture::getLNMIConfigur
     return nullptr; // Not every core has to have one
 }
 
+// TODO: The case where a PIC interrupt is remapped to another IO APIC is not handled
+//       - If the system uses 2 IO APICs with 8 inputs each and the PIT GSI is remapped to pin 10
+//         the first IO APIC will be returned because the PIT has GSI0
 InterruptArchitecture::IoApicInformation *InterruptArchitecture::getIoApicInformation(GlobalSystemInterrupt gsi) {
     IoApicInformation *ioapic;
     for (uint32_t i = 0; i < ioPlatform->ioapics.size(); ++i) {
         ioapic = ioPlatform->ioapics.get(i);
-        if (gsi >= ioapic->gsiBase && gsi <= ioapic->gsiMax) {
+        if (gsi >= ioapic->gsiBase && gsi <= ioapic->gsiMax) { // TODO: INTIs have to be compared (remapping)
             return ioapic;
         }
     }
--- src/device/interrupt/InterruptArchitecture.h
+++ src/device/interrupt/InterruptArchitecture.h
@@ -3,32 +3,114 @@
 
 #include <cstdint>
 #include "ApicRegisterInterface.h"
-#include "GlobalSystemInterrupt.h"
 #include "kernel/log/Logger.h"
 
 /*
+ * InterruptArchitecture:
  * This class implements a way to access information about the running system's interrupt model.
  * It can be initialized from different backends, the existing one uses ACPI 1.0b.
  * Backends needed for increased compatibility would be newer ACPI versions (especially >= ACPI 2.0)
  * and Intel's MultiProcessor tables (for ancient systems that don't support ACPI at all).
- *
- * TODO: I hate this, but separation is also weird since semantically it fits
  * Some values stored in this class have to be read from memory mapped APIC registers.
- * These will be initialized by the local APIC or IO APIC itself.
+ * These will be set when initializing the local APICs and IO APICs.
+ *
+ * GlobalSystemInterrupt:
+ * The 8295 PIC is usually used in a master-slave configuration with 2 PICs, so a PC/AT compatible system
+ * always support at least 16 hardware interrupts (including the PIC cascade).
+ * Modern systems using the APIC interrupt architecture can support a variable amount (and significantly more).
+ * To decouple the firmware/OS from the physical interrupt architecture, ACPI introduces "Global System Interrupts".
+ * GSIs only abstract hardware interrupts (no exceptions/faults) and start at 0!
+ * Because PC/AT compatibility is maintained, the first 16 (GSIs 0 - 15) global system interrupts are identity
+ * mapped to the PIC hardware interrupts.
+ * This introduces a problem, because the APIC architecture doesn't enforce how devices are physically wired
+ * to the IO APIC's interrupt inputs.
+ * To solve this, ACPI provides "Interrupt Source Overrides" in the MADT, which specify variances
+ * between PIC and APIC hardware interrupt configurations.
+ *
+ * Technically the notion of "GSIs" only got introduced in some ACPI version after 1.0b,
+ * but I see no problem with using it globally (even when ACPI/APIC is not available).
  */
 
 namespace Device {
 
 class InterruptArchitecture {
-    friend class Pic;
-    friend class LApic;
-    friend class IoApic;
-    friend class ApicTimer;
+    friend class LApic; // Initializes some values
+    friend class IoApic; // Initializes some values
 
 public:
     enum IntArch : uint8_t {
         PIC = 0,
         APIC = 1
+    };
+
+    // ! Global system interrupts
+
+    enum InterruptInput : uint8_t {};
+
+    // TODO: This class uses the outside class members of InterruptArchitecture, I don't like this
+    /**
+     * Global system interrupts abstract the hardware interrupt pins from the software.
+     * Supports conversion from/to vector numbers.
+     */
+    struct GlobalSystemInterrupt {
+        enum Gsi : uint8_t {
+            // PIC compatible GSIs
+            PIT = 0x00,
+            KEYBOARD = 0x01,
+            CASCADE = 0x02,
+            COM2 = 0x03,
+            COM1 = 0x04,
+            LPT2 = 0x05,
+            FLOPPY = 0x06,
+            LPT1 = 0x07,
+            RTC = 0x08,
+            MOUSE = 0x0C,
+            FPU = 0x0D,
+            PRIMARY_ATA = 0x0E,
+            SECONDARY_ATA = 0x0F,
+
+            // Other GSIs, currently none are used
+        };
+
+        GlobalSystemInterrupt() = default;
+        ~GlobalSystemInterrupt() = default;
+
+        explicit GlobalSystemInterrupt(uint8_t gsi); // From int because enums can be implicitly converted to int
+
+        /**
+         * Convert an interrupt vector number to a GlobalSystemInterrupt.
+         * GSIs are mapped to vector numbers 1:1 but translated by 32 (NOT influenced by IO APIC remappings!).
+         */
+        explicit GlobalSystemInterrupt(Kernel::InterruptDispatcher::Interrupt vector);
+
+        /**
+         * Convert the InterruptInput to a GlobalSystemInterrupt.
+         * This takes into account the IO APIC remappings.
+         */
+        explicit GlobalSystemInterrupt(InterruptInput inti);
+
+        operator Gsi() const; // To enum because enums can be implicitly converted to int
+
+        /**
+         * Convert the GlobalSystemInterrupt to an interrupt vector number.
+         * GSIs are mapped to vector numbers 1:1 but translated by 32 (NOT influenced by IO APIC remappings!).
+         */
+        explicit operator Kernel::InterruptDispatcher::Interrupt() const;
+
+        /**
+         * Convert the GlobalSystemInterrupt to an IO APIC interrupt input.
+         * This takes into account the IO APIC remappings.
+         */
+        explicit operator InterruptInput() const;
+
+        bool isValid();
+
+        // Convenient for iterating over GSIs
+        GlobalSystemInterrupt &operator++();
+
+    private:
+        Gsi gsi;
+        bool invalid; // This gets used for GSIs that are mapped to INTIs where another GSI was mapped to
     };
 
     // ! Processor local APIC architecture
@@ -45,7 +127,7 @@ public:
         uint8_t id; // Added for convenience (matches LApicConfiguration::id), 0xFF means all CPUs
         LVTEntry::PinPolarity polarity;
         LVTEntry::TriggerMode triggerMode;
-        uint8_t lint;
+        uint8_t lint; // Local APIC pin number
     };
 
     /**
@@ -75,7 +157,7 @@ public:
     struct IoInterruptOverride {
         uint8_t bus; // TODO: What is this
         GlobalSystemInterrupt source;
-        GlobalSystemInterrupt target;
+        InterruptInput target;
         REDTBLEntry::PinPolarity polarity;
         REDTBLEntry::TriggerMode triggerMode;
     };
@@ -92,8 +174,9 @@ public:
     struct IoPlatformInformation {
         uint8_t version; // Set by IoApic after MMIO is available
         bool eoiSupported; // Set by IoApic after MMIO is available
-        uint32_t irqToGsiMappings[16]; // GSIs for PIC IRQs (IRQ is the index)
-        uint8_t gsiToIrqMappings[16]; // Inverse of irqToGsiMappings
+        // TODO: This only allows to map PIC GSIs to INTIs in the range [0, 15] (use a hashmap or something?)
+        GlobalSystemInterrupt intiToGsiMappings[16]; // Inti is the index
+        InterruptInput gsiToIntiMappings[16]; // Gsi is the index
         GlobalSystemInterrupt globalGsiMax; // Systemwide max gsi
         Util::Data::ArrayList<IoApicInformation *> ioapics;
         Util::Data::ArrayList<IoInterruptOverride *> irqOverrides;
@@ -101,19 +184,40 @@ public:
     };
 
 public:
+    /**
+     * Initialize the InterruptArchitecture.
+     *
+     * @tparam T The backend to use
+     */
     template<typename T>
     static void initialize();
 
+    /**
+     * Ensures that the InterruptArchitecture has been initialized.
+     */
     static void verifyInitialized();
-    static bool hasApic();
-    static void verifyApic();
-    static IntArch getRunningArchitecture();
-    static void dumpPlatformInformation();
 
+    /**
+     * Ensure that the system runs with the APIC interrupt model.
+     */
+    static void verifyApic();
+
+    /**
+     * Determine if the system is currently running with the APIC interrupt model.
+     */
+    static bool hasApic() noexcept;
+
+    /**
+     * Determine the number of GSIs the system supports.
+     *
+     * @return The last supported GSI
+     */
     static GlobalSystemInterrupt getGlobalGsiMax();
 
     static Util::Data::ArrayList<LApicInformation *> &lapics();
     static Util::Data::ArrayList<IoApicInformation *> &ioapics();
+
+    static void dumpPlatformInformation();
 
 private:
     static LApicInformation *getLApicInformation(uint8_t lapicId);
@@ -136,6 +240,20 @@ void InterruptArchitecture::initialize() {
     Backend::initializeLPlatformInformation(localPlatform);
     Backend::initializeIoPlatformInformation(ioPlatform);
 
+    // Initialize mappings
+    for (uint8_t i = 0; i < 16; ++i) {
+        ioPlatform->gsiToIntiMappings[i] = static_cast<InterruptInput>(i);
+        ioPlatform->intiToGsiMappings[i] = static_cast<GlobalSystemInterrupt>(i);
+    }
+    for (auto *mapping : ioPlatform->irqOverrides) {
+        ioPlatform->gsiToIntiMappings[mapping->source] = mapping->target;
+        ioPlatform->intiToGsiMappings[mapping->target] = mapping->source;
+
+        // If e.g. the PIT (GSI0) is remapped to IO APIC INTI 2, the default mapping of GSI2 is no longer valid.
+        ioPlatform->gsiToIntiMappings[mapping->target] = static_cast<InterruptInput>(0xFF);
+        ioPlatform->intiToGsiMappings[mapping->source] = static_cast<GlobalSystemInterrupt>(0xFF);
+    }
+
     if (localPlatform->xApicSupported || localPlatform->x2ApicSupported) {
         runningArchitecture = APIC;
     } else {
@@ -144,6 +262,19 @@ void InterruptArchitecture::initialize() {
 
     initialized = true;
 }
+
+// TODO: Does this have a benefit to just move the structs out of the InterruptArchitecture class?
+//       - Yes, the structs can access the InterruptArchitecture members directly
+// The InterruptArchitecture defines some structures that should be in the Device namespace directly
+using InterruptInput = InterruptArchitecture::InterruptInput;
+using GlobalSystemInterrupt = InterruptArchitecture::GlobalSystemInterrupt;
+using LApicInformation = InterruptArchitecture::LApicInformation;
+using LNMIConfiguration = InterruptArchitecture::LNMIConfiguration;
+using LPlatformInformation = InterruptArchitecture::LPlatformInformation;
+using IoApicInformation = InterruptArchitecture::IoApicInformation;
+using IoInterruptOverride = InterruptArchitecture::IoInterruptOverride;
+using IoNMIConfiguration = InterruptArchitecture::IoNMIConfiguration;
+using IoPlatformInformation = InterruptArchitecture::IoPlatformInformation;
 
 }
 
