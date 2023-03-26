618940af4d318ea89639bfb6083bafcd41ba39b5
ChUrl <christoph.urlacher@protonmail.com>
2023-03-26 18:58:57 +0200

    Update the EditEvent base class to new single-buffer approach

--- src/application/edit/event/EditEvent.cpp
+++ src/application/edit/event/EditEvent.cpp
@@ -4,14 +4,12 @@
 
 #include "EditEvent.h"
 
-EditEvent::EditEvent(Util::Graphic::Ansi::CursorPosition cursor)
-        : eventSourcePosition(cursor), eventCreationTime(Util::Time::getSystemTime()) {}
+EditEvent::EditEvent(uint32_t cursor) : cursor(cursor), creation(Util::Time::getSystemTime()) {}
 
-void EditEvent::apply(FileBuffer &fileBuffer) {
-    if (!valid(fileBuffer)) {
-        // TODO: Make this less destructive
-        Util::Exception::throwException(Util::Exception::ILLEGAL_STATE, "Can't apply invalid EditEvent!");
-    }
-
-    applyAction(fileBuffer);
-}
+// void EditEvent::apply(FileBuffer &fileBuffer) {
+//     if (!valid(fileBuffer)) {
+//         Util::Exception::throwException(Util::Exception::ILLEGAL_STATE, "Can't apply invalid EditEvent!");
+//     }
+//
+//     applyAction(fileBuffer);
+// }
--- src/application/edit/event/EditEvent.h
+++ src/application/edit/event/EditEvent.h
@@ -7,11 +7,12 @@
 
 #include "lib/util/graphic/Ansi.h"
 #include "lib/util/time/Timestamp.h"
-#include "application/edit/FileBuffer.h"
+
+class CursorBuffer;
 
 class EditEvent {
 public:
-    explicit EditEvent(Util::Graphic::Ansi::CursorPosition cursor);
+    explicit EditEvent(uint32_t cursor);
 
     EditEvent(const EditEvent &copy) = delete;
 
@@ -23,17 +24,17 @@ public:
 
     ~EditEvent() = default;
 
-    void apply(FileBuffer &fileBuffer);
+    virtual void apply(CursorBuffer &cursorBuffer) = 0;
 
-    virtual void applyAction(FileBuffer &fileBuffer) = 0;
+    // virtual void applyAction(FileBuffer &fileBuffer) = 0;
 
-    [[nodiscard]] virtual bool valid(const FileBuffer &fileBuffer) const = 0;
+    // [[nodiscard]] virtual bool valid(const FileBuffer &fileBuffer) const = 0;
 
-    virtual void revert(FileBuffer &fileBuffer) = 0;
+    virtual void revert(CursorBuffer &cursorBuffer) = 0;
 
-private:
-    Util::Graphic::Ansi::CursorPosition eventSourcePosition;
-    Util::Time::Timestamp eventCreationTime;
+protected:
+    uint32_t cursor;
+    Util::Time::Timestamp creation;
 };
 
 #endif //HHUOS_EDITEVENT_H
