4499702057817fb1a8f75def43427277f856fa6c
ChUrl <christoph.urlacher@protonmail.com>
2023-04-07 10:26:15 +0200

    Simplify nixify.fish by using sd instead of sed

--- nixify.fish
+++ nixify.fish
@@ -3,7 +3,9 @@
 # NOTE: This works for TowBoot. Grub requires custom mkrescue paths...
 
 echo "Patching Shebangs"
-sed -i "s/#!\/bin\/bash/#!\/usr\/bin\/env\ bash/g" ./**.sh
+# sed -i "s/#!\/bin\/bash/#!\/usr\/bin\/env\ bash/g" ./**.sh
+sd '#!/bin/bash' '#!/usr/bin/env bash' ./**.sh
 
 echo "Patching /bin Paths"
-sed -i "s/\ \/bin\//\ \/run\/current-system\/sw\/bin\//g" ./cmake/**.txt
+# sed -i "s/\ \/bin\//\ \/run\/current-system\/sw\/bin\//g" ./cmake/**.txt
+sd ' /bin/' ' /run/current-system/sw/bin/' ./cmake/**.txt
